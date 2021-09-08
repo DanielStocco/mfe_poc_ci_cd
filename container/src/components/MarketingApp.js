edeb524a2de83a625452ee557188ebe9f5a9e73b
dstocco <daniel.stocco@incluit.com>
2021-09-08 11:26:34 -0300

    terminadas navegaciones

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,14 +1,24 @@
-import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 
 export default () => {
     //Esto no lo entendí
     const ref = useRef(null);
+    const history = useHistory();
+
+    const onNavigate = (location) => {
+        const { pathname: currentPathname } = history.location
+        const { pathname: nextPathName } = location;
+        if (currentPathname === nextPathName) { return }
+        history.push(nextPathName)
+    }
 
     useEffect( () => {
-        mount(ref.current);
-    })
+        const { onParentNavigate } = mount(ref.current, { onNavigate });
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref}/>
 }
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
 import App from './App'
 
-const mount = (element) => {
-    ReactDom.render(<App/>, element)
+const mount = (element, options) => {
+    console.log(options);
+    const history = options.defaultHistory || createMemoryHistory();
+
+    const onParentNavigate = (location) => {
+        const { pathname: currentPathname } = history.location
+        const { pathname: nextPathName } = location;
+        if (currentPathname === nextPathName) { return }
+
+        history.push(nextPathName)
+    }
+
+    if (options.onNavigate) {
+        history.listen(options.onNavigate)
+    }
+
+    ReactDom.render(<App history={ history }/>, element)
+
+    return { onParentNavigate }
 };
 
 if(process.env.NODE_ENV === 'development') {
     const rootElement = document.getElementById('_marketing-dev-root');
-    if (rootElement) { mount(rootElement); }
+    if (rootElement) {
+        mount(rootElement, { defaultHistory: createBrowserHistory() });
+    }
 }
 
 export { mount };
